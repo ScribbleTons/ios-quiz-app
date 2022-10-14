7f8defcbbe908ad5ff5c7b29a331ec10622fdef1
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2022-10-14 21:12:02 +0100

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Emmanuel Ugwuoke on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
